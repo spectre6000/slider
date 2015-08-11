bb8434bd5af70ce384dab3c771aeaf9f1131b34d
spectre6000 <spectre6000@gmail.com>
2015-08-11 14:07:41 -0600

    everything but the dots

--- slider.js
+++ slider.js
@@ -1,23 +1,26 @@
 $(function(){
   
+    //DOM
+    var $slider = $("#content");
+    var $gallery = $slider.find("#gallery");
+    var $photos = $gallery.find('.photo');
+    var $dots = $slider.find(".dot");
+    var $next = $slider.find("#right");
+    var $last = $slider.find("#left");
+
     //config
     var width = 1000;
     var speed = 1000;
     var duration = 2000;
     var current = 1;
     var currentDot = 2;
+    var photoCount = $photos.length-1;
 
     //variables
     var period;
     var next = '-=';
     var last = '+=';
 
-    //DOM
-    var $slider = $("#content");
-    var $gallery = $slider.find("#gallery");
-    var $photos = $gallery.find('.photo');
-    var $dots = $slider.find(".dot");
-    
 
     //mechanism
     function start(direction) {
@@ -25,22 +28,38 @@ $(function(){
     }
 
     function advance(direction) {
+      if($gallery.is(':animated')) return;
 
-      $("#dot" + current).animate({"opacity": 0.4}, speed);
-      $("#dot" + currentDot).animate({"opacity": 1.0}, speed);
+      // $("#dot" + current).animate({"opacity": 0.4}, speed);
+      // $("#dot" + currentDot).animate({"opacity": 1.0}, speed);
+
+      // $('.dot').animate({"opacity": 0.4}, speed);
+      // $('#dot' + current).animate({"opacity": 1}, speed);
+
+      if (current === 0 ) {
+        current = photoCount -1;
+        $gallery.css({'margin-left': '-5000px'});
+      };
 
       $gallery.animate({'margin-left': direction+width}, speed, function() {
-        
-        current ++;
-        currentDot ++;
-        
-        if (currentDot === $photos.length) {
-          currentDot = 1;
+
+        if (direction === next) {
+          current ++;
+          // currentDot ++;
+        } else {
+          current --;
+          // currentDot --;
         };
-        if (current === $photos.length) {
+        
+        // if (currentDot === photoCount) {
+        //   currentDot = 1;
+        // };
+      
+        if (current === photoCount) {
           current = 1;
-          $gallery.css({'margin-left': 0});
+          $gallery.css({'margin-left': '-1000px'});
         };
+
       });
     };
 
@@ -49,7 +68,8 @@ $(function(){
     }
 
     //activation
-    $slider.on('mouseenter', pause).on('mouseleave', function() {start(next)} );
-
     start(next);
+    $slider.on('mouseenter', pause).on('mouseleave', function() {start(next)} );
+    $next.click(function() {advance(next)});
+    $last.click(function() {advance(last)});
 });
